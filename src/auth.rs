7372deadc7de3aa5052f3c13330d37dc0ae116bc
frkri <57220519+frkri@users.noreply.github.com>
2024-03-06 10:05:49 +0100

    Optimize imports

--- src/auth.rs
+++ src/auth.rs
@@ -11,7 +11,7 @@ use base64ct::Encoding;
 use password_hash::rand_core::OsRng;
 use password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString};
 use rand::RngCore;
-use sqlx::{Encode, SqlitePool};
+use sqlx::{SqlitePool};
 
 use crate::error::ModelResult;
 use crate::models::api::ApiClient;
